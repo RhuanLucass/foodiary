61d6288931ff3d4e0965a22716cfa77012bd101d
Rhuan Lucas <rhuanfernandes.rf@gmail.com>
2026-01-15 22:09:15 -0300

    feat: adicionar suporte a fontes personalizadas e atualizar dependências

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,25 @@
+import { LogOutIcon } from 'lucide-react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { colors } from '../styles/colors';
+
+export function HomeHeader() {
+  return (
+    <View className=" bg-lime-400">
+      <SafeAreaView className="flex-row items-center justify-between px-4 pt-3">
+        <View>
+          <Text className="font-sans-regular text-sm text-gray-700">
+            Olá, 👋
+          </Text>
+          <Text className="font-sans-semibold text-base text-black-700">
+            Rhuan
+          </Text>
+        </View>
+
+        <TouchableOpacity className="size-12 items-center justify-center">
+          <LogOutIcon size={20} color={colors.black[700]} />
+        </TouchableOpacity>
+      </SafeAreaView>
+    </View>
+  );
+}
